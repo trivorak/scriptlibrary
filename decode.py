38e94de91bb1109b271aee1629903c6e19f18118
Drake Champagne <trivorak@gmail.com>
2022-03-11 10:40:21 -0600

    tabs/spaces
    spaces vs tab ... who knows... who cares. Keep changing preference

--- decode.py
+++ decode.py
@@ -1,17 +1,17 @@
 # Decode Key is always the last 2 digits of the hex string
 
-inputString = input("What's your encoded String? {Keep on a Single Line} ")
+inputString = input("What's your encoded String? {Keep on a Single Line} \n")
 encodedString = []
 decodedString = [] 
 
 def getKey(input):
-	return input[len(input)-2:len(input)]
+    return input[len(input)-2:len(input)]
 
 key = getKey(inputString)
 
 # Split Each Digit
 for i in range(0,len(inputString)):
-	encodedString.append(inputString[i:i+1])
+    encodedString.append(inputString[i:i+1])
 
 # Strip key for array
 encodedString.pop(-1)
@@ -19,9 +19,9 @@ encodedString.pop(-1)
 
 # Decode Loop
 for element in encodedString:
-	decodedString.append(hex((int(element,16) - int(key,16))% 16)[2:])
+    decodedString.append(hex((int(element,16) - int(key,16))% 16)[2:])
 
-# Squash it all together	
+# Squash it all together
 squashedList = "".join(decodedString)
 
 # Make all digits uppercase
@@ -31,4 +31,4 @@ print("")
 
 # print sting in 32 character length
 for x in range(0,len(squashedList),32):
-	print(squashedList[x:x+32])
+    print(squashedList[x:x+32])
--- encode.py
+++ encode.py
@@ -7,18 +7,18 @@ convertedList = []
 randomSeed = randint(1,15)
 
 #Input Variables (For Testing)
-inputstr = input("What's Your Input String? {Please keep on a single line} ")
+inputstr = input("What's Your Input String? {Please keep on a single line} \n")
 
 #Debug
 print(len(inputstr))
 
 # Split each "digit" as an index
 for i in range(0,len(inputstr)):
-	inputList.append(inputstr[i:i+1])
-	
-# Encoding Process	
+    inputList.append(inputstr[i:i+1])
+
+# Encoding Process
 for element in inputList:
-	convertedList.append(hex((int(element,16) + randomSeed)% 16)[2:])
+    convertedList.append(hex((int(element,16) + randomSeed)% 16)[2:])
 
 # Attach Random Seed Int used in Offset with a 0 in front (for 2 digit hex format)
 convertedList.append(str(0)+hex(randomSeed)[2:])
@@ -31,4 +31,4 @@ print("")
 
 # print sting in 32 character length
 for x in range(0,len(squashedList),32):
-	print(squashedList[x:x+32])
+    print(squashedList[x:x+32])
